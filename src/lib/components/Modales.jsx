fc3f35f36619f06b28ad98a472ca25d36ed11576
ERIC CLERMONTET <emc@go4.social>
2023-02-27 16:42:40 +0100

    03 Adding CSS animation

--- src/lib/components/Modales.jsx
+++ src/lib/components/Modales.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import close from './assets/close.svg'
-import { Content, IMG, ModaleContent, ModaleDiv } from './styles/modale'
+import styled from 'styled-components'
 
 const Modale = (props) => {
   return (
@@ -14,3 +14,42 @@ const Modale = (props) => {
 }
 
 export default Modale
+
+const ModaleDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: auto;
+  height: auto;
+  z-index: 10;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.8);
+`
+const ModaleContent = styled.div`
+  width: auto;
+  height: auto;
+  position: absolute;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.4);
+  padding: 1rem;
+  border-radius: .5rem;
+  background-color: white;
+  top: 40vh;
+`
+const Content = styled.p`
+  text-align: center;
+  color: black;
+`
+export const IMG = styled.img`
+  position: absolute;
+  top: -0.8rem;
+  right: -0.8rem;
+  width: 1.6rem;
+  height: 1.6rem;
+  cursor: pointer;
+`
+
